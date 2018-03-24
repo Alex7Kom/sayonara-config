15dce2bf9a9a90fb523541f784446b6bbb736c28
Alexey Komarov <alex7kom@gmail.com>
2018-03-24 20:47:53 +0300

    add --staged flag for pretty

--- src/scripts/pretty.js
+++ src/scripts/pretty.js
@@ -1,23 +1,43 @@
 'use strict';
 
 const fs = require('fs');
-const path = require('path');
+const childProcess = require('child_process');
 
 const glob = require('glob');
 const prettier = require('prettier');
 
 const prettierConfig = require('../configs/prettier');
 
-const files = glob.sync(path.join(process.cwd(), 'src/**/*.js'), {});
+const isStaged = process.argv[3] === '--staged';
+
+const files = glob.sync('src/**/*.js', {});
+let stagedFiles;
+
+if (isStaged) {
+  stagedFiles = childProcess.execSync('git diff --name-only --cached', {
+    encoding: 'utf8'
+  });
+  stagedFiles = stagedFiles.split('\n');
+}
 
 files.forEach(filePath => {
+  if (isStaged && stagedFiles.indexOf(filePath) === -1) {
+    return;
+  }
+
   const fileContent = fs.readFileSync(filePath, 'utf8');
 
-  if (!prettier.check(fileContent, prettierConfig)) {
-    console.log(`${filePath} is pretty now!`);
+  if (prettier.check(fileContent, prettierConfig)) {
+    return console.log(`${filePath} is already pretty!`);
+  }
 
-    const fixedContent = prettier.format(fileContent, prettierConfig);
+  console.log(`${filePath} is pretty now!`);
 
-    fs.writeFileSync(filePath, fixedContent);
+  const fixedContent = prettier.format(fileContent, prettierConfig);
+
+  fs.writeFileSync(filePath, fixedContent);
+
+  if (isStaged) {
+    childProcess.execSync('git add ' + filePath);
   }
 });
