14e8ec980983df5468d11b731eb8fd3047fc1897
Alexey Komarov <alex7kom@gmail.com>
2018-07-14 22:25:16 +0300

    add eslint override for Jest

--- src/blocks/jest/index.js
+++ src/blocks/jest/index.js
@@ -22,6 +22,8 @@ function addJest() {
   addJestConfig();
 
   addTestCommand();
+
+  addEslintOverride();
 }
 
 function addTestCommand() {
@@ -40,6 +42,35 @@ function addJestConfig() {
   const rcFile = 'jest.config.js';
   const templatePath = path.join(__dirname, 'jest.config.tpl.js');
   addFileFromTemplate(rcFile, templatePath);
+}
+
+function addEslintOverride() {
+  const jestOverride = {
+    files: ['*.test.js', '*.spec.js'],
+    env: {
+      jest: true
+    }
+  };
+
+  updatePackageInfo(packageInfo => {
+    if (!packageInfo.eslintConfig) {
+      packageInfo.eslintConfig = {};
+    }
+
+    if (packageInfo.eslintConfig.overrides) {
+      const hasOverride = packageInfo.eslintConfig.overrides.some(
+        override => override.files.join() === jestOverride.files.join()
+      );
+
+      if (!hasOverride) {
+        packageInfo.eslintConfig.overrides.push(jestOverride);
+      }
+    } else {
+      packageInfo.eslintConfig.overrides = [jestOverride];
+    }
+
+    return packageInfo;
+  });
 }
 
 module.exports = addJest;
