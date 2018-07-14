fe2cdc46d85070e13d5c058fb330f7c74f19544c
Alexey Komarov <alex7kom@gmail.com>
2018-07-14 21:37:38 +0300

    add Jest if no test command found

--- src/blocks/jest/index.js
+++ src/blocks/jest/index.js
@@ -13,6 +13,7 @@ function addJest() {
 
   if (
     packageInfo.scripts &&
+    packageInfo.scripts.test &&
     packageInfo.scripts.test !== 'echo "Error: no test specified" && exit 1'
   ) {
     return;
