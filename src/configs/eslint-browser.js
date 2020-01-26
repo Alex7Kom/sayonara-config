5bee17759e7d937c058fd96c6d7e275f61313228
Alexey Komarov <alex7kom@gmail.com>
2020-01-26 11:58:21 +0300

    add browser-library set

---
+++ src/configs/eslint-browser.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const prettierConfig = require('./prettier');
+const baseRules = require('./includes/eslint-base-rules');
+
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true
+  },
+  parserOptions: {
+    ecmaVersion: 5
+  },
+  plugins: ['prettier'],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  rules: Object.assign(
+    {
+      'prettier/prettier': ['error', prettierConfig]
+    },
+    baseRules
+  )
+};
