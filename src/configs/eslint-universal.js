f0b3b4401591898ec7f4350e3810d1b4eb3c7384
Alexey Komarov <alex7kom@gmail.com>
2020-02-02 18:52:49 +0300

    universal libraries should now be commonjs

--- src/configs/eslint-universal.js
+++ src/configs/eslint-universal.js
@@ -4,6 +4,9 @@ const prettierConfig = require('./prettier');
 const baseRules = require('./includes/eslint-base-rules');
 
 module.exports = {
+  env: {
+    commonjs: true
+  },
   parserOptions: {
     ecmaVersion: 5
   },
