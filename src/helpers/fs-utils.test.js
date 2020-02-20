c13bc33344d46469259a39e063a7003be5908ad3
Alexey Komarov <alex7kom@gmail.com>
2020-02-20 18:48:42 +0300

    add tests for fs-utils and json-config

---
+++ src/helpers/fs-utils.test.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const fs = require('fs');
+
+const { addFileFromTemplate, updateConfigFile } = require('./fs-utils');
+
+jest.mock('fs');
+
+beforeEach(() => jest.clearAllMocks());
+
+describe('fsUtils', () => {
+  describe('addFileFromTemplate', () => {
+    it('skips file if it already exists', () => {
+      fs.readFileSync.mockImplementation(() => 'exists');
+      fs.writeFileSync.mockImplementation(() => null);
+
+      addFileFromTemplate('foo', 'template');
+
+      expect(fs.writeFileSync.mock.calls).toHaveLength(0);
+    });
+
+    it('writes file if it does not exist', () => {
+      fs.readFileSync.mockImplementation(filePath => {
+        if (filePath === 'foo') throw new Error('does not exist');
+        if (filePath === 'template') return 'contents';
+      });
+      fs.writeFileSync.mockImplementation(() => null);
+
+      addFileFromTemplate('foo', 'template');
+
+      expect(fs.writeFileSync.mock.calls[0]).toEqual(['foo', 'contents']);
+    });
+  });
+
+  describe('updateConfigFile', () => {
+    it('adds file by template if it does not exist', () => {
+      fs.readFileSync.mockImplementation(filePath => {
+        if (filePath === 'foo') throw new Error('does not exist');
+        if (filePath === 'template') return 'foo=bar';
+      });
+      fs.writeFileSync.mockImplementation(() => null);
+
+      updateConfigFile('foo', 'template');
+
+      expect(fs.writeFileSync.mock.calls[0]).toEqual([
+        'foo',
+        'foo=bar\n## DO NOT EDIT ABOVE THIS LINE ##\n'
+      ]);
+    });
+
+    it('upgrades file to template if it does exist', () => {
+      fs.readFileSync.mockImplementation(filePath => {
+        if (filePath === 'foo') return 'config=exists';
+        if (filePath === 'template') return 'foo=bar';
+      });
+      fs.writeFileSync.mockImplementation(() => null);
+
+      updateConfigFile('foo', 'template');
+
+      expect(fs.writeFileSync.mock.calls[0]).toEqual([
+        'foo',
+        'foo=bar\n## DO NOT EDIT ABOVE THIS LINE ##\nconfig=exists'
+      ]);
+    });
+
+    it('updates file by template', () => {
+      fs.readFileSync.mockImplementation(filePath => {
+        if (filePath === 'foo') {
+          return 'old=config\n## DO NOT EDIT ABOVE THIS LINE ##\nconfig=exists';
+        }
+
+        if (filePath === 'template') return 'foo=bar';
+      });
+      fs.writeFileSync.mockImplementation(() => null);
+
+      updateConfigFile('foo', 'template');
+
+      expect(fs.writeFileSync.mock.calls[0]).toEqual([
+        'foo',
+        'foo=bar\n## DO NOT EDIT ABOVE THIS LINE ##\nconfig=exists'
+      ]);
+    });
+  });
+});
---
+++ src/helpers/json-config.test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const fs = require('fs');
+
+const JSONConfig = require('./json-config');
+
+jest.mock('fs');
+
+beforeEach(() => jest.clearAllMocks());
+
+describe('JSONConfig.updateJSONConfig', () => {
+  it('reads config file, calls updater and writes the result', () => {
+    fs.readFileSync.mockImplementation(() => '{"a":1}');
+    fs.writeFileSync.mockImplementation(() => null);
+
+    JSONConfig.updateJSONConfig('foo', x => {
+      x.a = 2;
+
+      return x;
+    });
+
+    expect(fs.writeFileSync.mock.calls[0][1]).toBe('{\n  "a": 2\n}\n');
+  });
+
+  it('does not write anything if updater returned false', () => {
+    fs.readFileSync.mockImplementation(() => '{"a":1}');
+    fs.writeFileSync.mockImplementation(() => null);
+
+    JSONConfig.updateJSONConfig('foo', () => false);
+
+    expect(fs.writeFileSync.mock.calls).toHaveLength(0);
+  });
+});
