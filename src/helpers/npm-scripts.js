5576d09e30aa49792b70f4df2330162b142e4c09
Alexey Komarov <alex7kom@gmail.com>
2020-02-21 20:02:08 +0300

    add npm-scripts helper

---
+++ src/helpers/npm-scripts.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const { updatePackageInfo } = require('./package-json');
+
+function addNpmScript(name, command) {
+  updatePackageInfo(packageInfo => {
+    packageInfo.scripts[name] = command;
+
+    return packageInfo;
+  });
+}
+
+function removeNpmScript(name) {
+  updatePackageInfo(packageInfo => {
+    delete packageInfo.scripts[name];
+
+    return packageInfo;
+  });
+}
+
+module.exports = {
+  addNpmScript,
+  removeNpmScript
+};
---
+++ src/helpers/npm-scripts.test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const { updatePackageInfo } = require('./package-json');
+
+const { addNpmScript, removeNpmScript } = require('./npm-scripts');
+
+jest.mock('./package-json');
+
+beforeEach(() => jest.clearAllMocks());
+
+describe('addNpmScript', () => {
+  it('adds script with specified name and command', () => {
+    updatePackageInfo.mockImplementation(cb => cb({ scripts: {} }));
+
+    addNpmScript('test', 'jest');
+
+    expect(updatePackageInfo.mock.results[0].value).toEqual({
+      scripts: {
+        test: 'jest'
+      }
+    });
+  });
+});
+
+describe('removeNpmScript', () => {
+  it('removes script with specified name', () => {
+    updatePackageInfo.mockImplementation(cb =>
+      cb({ scripts: { test: 'jest' } })
+    );
+
+    removeNpmScript('test');
+
+    expect(updatePackageInfo.mock.results[0].value).toEqual({
+      scripts: {}
+    });
+  });
+});
