d2be42244578c494737e4c802d8682cc100a7cb4
Alexey Komarov <alex7kom@gmail.com>
2020-03-13 19:39:49 +0300

    disable eslint node rules related to import in eslint-jest

--- src/configs/eslint-jest.js
+++ src/configs/eslint-jest.js
@@ -6,5 +6,10 @@ module.exports = {
     'plugin:jest/recommended',
     'plugin:jest/style',
     'plugin:jest-formatting/recommended'
-  ]
+  ],
+  rules: {
+    'node/no-unpublished-import': 'off',
+    'node/no-missing-import': 'off',
+    'node/no-missing-require': 'off'
+  }
 };
